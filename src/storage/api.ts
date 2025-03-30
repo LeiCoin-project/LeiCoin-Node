473829816d142665d8914bf939928cd5a0c1ca63
LeiCraft_ <88381277+LeiCraft@users.noreply.github.com>
2025-03-30 12:19:08 +0000

    update

--- src/storage/api.ts
+++ src/storage/api.ts
@@ -1,8 +1,10 @@
+import { FastEvents } from "@leicoin/utils/fastevents";
 import { BlockDB } from "./blocks";
 import { ChainstateStore } from "./chainstate";
 import { type SmartContractStateDB } from "./smart-contract-state";
 import { MinterDB } from "./state/minters";
 import { WalletDB } from "./state/wallets";
+import type { Block } from "@leicoin/common/models/block";
 
 export class StorageAPI {
 
@@ -11,7 +13,10 @@ export class StorageAPI {
         readonly wallets: WalletDB,
         readonly minters: MinterDB,
         readonly scstates: SmartContractStateDB,
-        readonly chainstate: ChainstateStore
+        readonly chainstate: ChainstateStore,
+        readonly events = new FastEvents.Emitter([
+            new FastEvents.Topic<"block", [Block]>("block")
+        ])
     ) {}
 
 }
